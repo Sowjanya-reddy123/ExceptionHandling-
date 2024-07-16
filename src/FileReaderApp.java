74dc3d50a70f847ceea1db2f4e7c9691a1cf8455
Sowjanya-reddy123 <BodduSowjanya@Vebytechlabs.onmicrosoft.com>
2024-07-16 17:32:42 +0530

    added FileReaderApp class

---
+++ src/FileReaderApp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FileReaderApp {
+    private Scanner scanner;
+
+    public FileReaderApp (Scanner scanner){
+        this.scanner=scanner;
+    }
+
+    public void readFile(String filepath) {
+
+    }
+}
